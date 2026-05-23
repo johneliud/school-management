f3335a51568e360e82b1ab51dc70b4bd582a147f
John <johneliud4@gmail.com>
2026-05-24 00:45:41 +0300

    feat: add StudentNotFound and NotAuthorized error variants to school management contract.

--- contracts/school-management/src/error.rs
+++ contracts/school-management/src/error.rs
@@ -4,4 +4,6 @@ use soroban_sdk::contracterror;
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum ContractError {
     InsufficientFunds = 1,
+    StudentNotFound = 2,
+    NotAuthorized = 3,
 }
